53ff04ed7f6647bd1db616241a9f0e80a82aaa52
Mattia Martelli <mattia.martelli@live.it>
2022-04-06 22:18:51 +0200

    Doc for AssistantCard,  simpler method in Player, removed a decorator

--- src/main/java/it/polimi/ingsw/model/AssistantCard.java
+++ src/main/java/it/polimi/ingsw/model/AssistantCard.java
@@ -1,3 +1,6 @@
 package it.polimi.ingsw.model;
 
+/**
+ * Data type dedicated to storing information regarding the assistant cards
+ */
 public record AssistantCard(int cardValue, int movementPoints) {}
--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -11,7 +11,7 @@ public class Player {
         assistantDeck = new AssistantCard[10];
 
         for (int i = 0; i < 10; ++i) {
-            assistantDeck[i] = new AssistantCard(i + 1, (i + 1) % 2 == 0 ? (i + 1) / 2 : (i + 2) / 2);
+            assistantDeck[i] = new AssistantCard(i + 1, (i / 2) + 1);
         }
     }
 
