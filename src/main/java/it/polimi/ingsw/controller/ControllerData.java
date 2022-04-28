6a5913495b7f1099fde7d4838a6e7ecc54fa57b6
Mattia Martelli <mattia.martelli@live.it>
2022-04-28 21:37:08 +0200

    Added regions to ControllerData

--- src/main/java/it/polimi/ingsw/controller/ControllerData.java
+++ src/main/java/it/polimi/ingsw/controller/ControllerData.java
@@ -10,6 +10,9 @@ import java.util.Map;
  * @author Mattia Martelli
  */
 public class ControllerData {
+
+    // region Fields
+
     private static ControllerData             instance;
     private        GameModel                  gameModel;
     private        Map<Player, AssistantCard> playerAssistantCardMap;
@@ -18,6 +21,167 @@ public class ControllerData {
     private        boolean                    hasPlayedCard;
     private        Integer                    numOfPlayers;
     private        boolean                    expertMode;
+
+    // endregion
+
+    // region Getters
+
+    /**
+     * Returns the currently instanciated GameModel
+     * @return The pointer to the GameModel
+     */
+    public GameModel getGameModel() {
+        return gameModel;
+    }
+
+    /**
+     * Returns the assistant card played by the requested player
+     * @param player A pointer to the player that used the card
+     * @return The AssistantCard played
+     */
+    public AssistantCard getPlayerCard(Player player) {
+        return playerAssistantCardMap.get(player);
+    }
+
+    /**
+     * Returns the players' play order
+     * @return An array containing the play order
+     */
+    public Player[] getPlayersOrder() {
+        return Arrays.copyOf(playersOrder, playersOrder.length);
+    }
+
+    /**
+     * Return the player corresponding to the requested index
+     * @param index The index of the player to return
+     * @return A pointer to the requested player
+     */
+    public Player getPlayersOrder(int index) {
+        return playersOrder[index];
+    }
+
+    /**
+     * Returns the trigger value
+     * @return A boolean representing the trigger
+     */
+    public boolean getTrigger() {
+        return trigger;
+    }
+
+    /**
+     * Check whether the current player has played a card
+     * @return A boolean representing the value
+     */
+    public boolean checkPlayedCard() {
+        return hasPlayedCard;
+    }
+
+    /**
+     * Return the number of players of the current game
+     * @return The number of players
+     */
+    public int getNumOfPlayers() {
+        return numOfPlayers;
+    }
+
+    /**
+     * Returns the value of the expert mode flag
+     * @return The flag value
+     */
+    public boolean getExpertMode() {
+        return expertMode;
+    }
+
+    // endregion
+
+    // region Setters
+
+    /**
+     * Set the GameModel instance
+     * @param gameModel A pointer to the GameModel's instance
+     */
+    public void setGameModel(GameModel gameModel) {
+        this.gameModel = gameModel;
+    }
+
+    /**
+     * Set the relation Player -> AssistantCard
+     * @param playerAssistantCardMap A Map representing the relation
+     */
+    public void setPlayerAssistantCardMap(Map<Player, AssistantCard> playerAssistantCardMap) {
+        this.playerAssistantCardMap = playerAssistantCardMap;
+    }
+
+    /**
+     * Cleans the map Player -> AssistantCard
+     */
+    public void nukePlayerAssistantCardMap() {
+        playerAssistantCardMap = null;
+    }
+
+    /**
+     * Add a tuple (Player, AssistantCard) to the map
+     * @param player The Player that chose the card
+     * @param assistantCard The chosen card
+     */
+    public void addPlayerCard(Player player, AssistantCard assistantCard) {
+        playerAssistantCardMap.put(player, assistantCard);
+    }
+
+    /**
+     * Set the order the players will follw
+     * @param playersOrder An array containing the play order
+     */
+    public void setPlayersOrder(Player[] playersOrder) {
+        this.playersOrder = Arrays.copyOf(playersOrder, playersOrder.length);
+    }
+
+    /**
+     * Set the generic trigger to true
+     */
+    public void setTrigger() {
+        trigger = true;
+    }
+
+    /**
+     * Set the generic trigger to false
+     */
+    public void resetTrigger() {
+        trigger = false;
+    }
+
+    /**
+     * Set the trigger that represents whether the current player has played a card to true
+     */
+    public void setPlayedCard() {
+        hasPlayedCard = true;
+    }
+
+    /**
+     * Set the trigger that represents whether the current player has played a card to true
+     */
+    public void resetPlayedCard() {
+        hasPlayedCard = false;
+    }
+
+    /**
+     * Set the number of players of the current game
+     * @param numOfPlayers The number of players
+     */
+    public void setNumOfPlayers(int numOfPlayers) {
+        this.numOfPlayers = numOfPlayers;
+    }
+
+    /**
+     * Set the expert mode flag to true
+     */
+    public void setExpertMode() {
+        expertMode = true;
+    }
+
+    // endregion
+
+    // region Instance related methods
 
     private ControllerData() {
         expertMode = false;
@@ -53,152 +217,5 @@ public class ControllerData {
         instance = null;
     }
 
-    /**
-     * Returns the currently instanciated GameModel
-     * @return The pointer to the GameModel
-     */
-    public GameModel getGameModel() {
-        return gameModel;
-    }
-
-    /**
-     * Set the GameModel instance
-     * @param gameModel A pointer to the GameModel's instance
-     */
-    public void setGameModel(GameModel gameModel) {
-        this.gameModel = gameModel;
-    }
-
-    /**
-     * Set the relation Player -> AssistantCard
-     * @param playerAssistantCardMap A Map representing the relation
-     */
-    public void setPlayerAssistantCardMap(Map<Player, AssistantCard> playerAssistantCardMap) {
-        this.playerAssistantCardMap = playerAssistantCardMap;
-    }
-
-    /**
-     * Add a tuple (Player, AssistantCard) to the map
-     * @param player The Player that chose the card
-     * @param assistantCard The chosen card
-     */
-    public void addPlayerCard(Player player, AssistantCard assistantCard) {
-        playerAssistantCardMap.put(player, assistantCard);
-    }
-
-    /**
-     * Returns the assistant card played by the requested player
-     * @param player A pointer to the player that used the card
-     * @return The AssistantCard played
-     */
-    public AssistantCard getPlayerCard(Player player) {
-        return playerAssistantCardMap.get(player);
-    }
-
-    /**
-     * Set the order the players will follw
-     * @param playersOrder An array containing the play order
-     */
-    public void setPlayersOrder(Player[] playersOrder) {
-        this.playersOrder = Arrays.copyOf(playersOrder, playersOrder.length);
-    }
-
-    /**
-     * Returns the players' play order
-     * @return An array containing the play order
-     */
-    public Player[] getPlayersOrder() {
-        return Arrays.copyOf(playersOrder, playersOrder.length);
-    }
-
-    /**
-     * Return the player corresponding to the requested index
-     * @param index The index of the player to return
-     * @return A pointer to the requested player
-     */
-    public Player getPlayersOrder(int index) {
-        return playersOrder[index];
-    }
-
-    /**
-     * Cleans the map Player -> AssistantCard
-     */
-    public void nukePlayerAssistantCardMap() {
-        playerAssistantCardMap = null;
-    }
-
-    /**
-     * Set the generic trigger to true
-     */
-    public void setTrigger() {
-        trigger = true;
-    }
-
-    /**
-     * Set the generic trigger to false
-     */
-    public void resetTrigger() {
-        trigger = false;
-    }
-
-    /**
-     * Returns the trigger value
-     * @return A boolean representing the trigger
-     */
-    public boolean getTrigger() {
-        return trigger;
-    }
-
-    /**
-     * Set the trigger that represents whether the current player has played a card to true
-     */
-    public void setPlayedCard() {
-        hasPlayedCard = true;
-    }
-
-    /**
-     * Set the trigger that represents whether the current player has played a card to true
-     */
-    public void resetPlayedCard() {
-        hasPlayedCard = false;
-    }
-
-    /**
-     * Check whether the current player has played a card
-     * @return A boolean representing the value
-     */
-    public boolean checkPlayedCard() {
-        return hasPlayedCard;
-    }
-
-    /**
-     * Set the number of players of the current game
-     * @param numOfPlayers The number of players
-     */
-    public void setNumOfPlayers(int numOfPlayers) {
-        this.numOfPlayers = numOfPlayers;
-    }
-
-    /**
-     * Return the number of players of the current game
-     * @return The number of players
-     */
-    public int getNumOfPlayers() {
-        return numOfPlayers;
-    }
-
-    /**
-     * Set the expert mode flag to true
-     */
-    public void setExpertMode() {
-        expertMode = true;
-    }
-
-    /**
-     * Returns the value of the expert mode flag
-     * @return The flag value
-     */
-    public boolean getExpertMode() {
-        return expertMode;
-    }
+    // endregion
 }
