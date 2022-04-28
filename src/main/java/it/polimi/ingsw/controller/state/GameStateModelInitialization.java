6061bfc7e62b5f3a7067aadfa694fc59e815a0e2
Mattia Martelli <mattia.martelli@live.it>
2022-04-28 21:25:45 +0200

    Added missing data creation to GameStateModelInitialization

--- src/main/java/it/polimi/ingsw/controller/state/GameStateModelInitialization.java
+++ src/main/java/it/polimi/ingsw/controller/state/GameStateModelInitialization.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.controller.state;
 
 import it.polimi.ingsw.controller.ControllerData;
-import it.polimi.ingsw.model.GameModel;
+import it.polimi.ingsw.model.*;
 
 /**
  * State representing the initial model initialization
@@ -15,5 +15,50 @@ public class GameStateModelInitialization implements GameStateSetup {
     public void executeState() {
         ControllerData data = ControllerData.getInstance();
         data.setGameModel(new GameModel(data.getNumOfPlayers(), data.getExpertMode()));
+
+        GameModel model   = data.getGameModel();
+        Player[]  players = ControllerData.getInstance().getPlayersOrder();
+        for (int i = 0; i < players.length; ++i)
+            model.setPlayer(players[i], i);
+
+        if (data.getNumOfPlayers() == 4)
+            setMembers(players);
+
+        if (data.getExpertMode())
+            setCoins(players, model);
+    }
+
+    /**
+     * Set up the teams (0, 2) and (1, 3)
+     */
+    private void setMembers(Player[] players) {
+        for (int i = 0; i < 4; ++i) {
+            if (players[i] instanceof PlayerTeam p)
+                p.setTeamMember(players[(i + 2) % 4]);
+
+            else if (players[i] instanceof PlayerTeamExpert p)
+                p.setTeamMember(players[(i + 2) % 4]);
+
+            else
+                throw new IllegalStateException("The players were not correctly created: use PlayerTeam or PlayerTeamExpert when there are four players");
+        }
+    }
+
+    /**
+     * Give a coin to each player, removing the corresponding amount from the model
+     */
+    private void setCoins(Player[] players, GameModel model) {
+        for (Player player : players) {
+            if (player instanceof PlayerExpert p)
+                p.setCoinCount(1);
+
+            else if ((player instanceof PlayerTeamExpert p))
+                p.setCoinCount(1);
+
+            else
+                throw new IllegalStateException("The players were not correctly created: use PlayerExpert or PlayerExpertTeam when using the expert ruleset");
+        }
+
+        model.decreaseCoinPool(players.length);
     }
 }
