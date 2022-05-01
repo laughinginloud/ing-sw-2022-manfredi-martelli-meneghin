41daa55b5dea0428e4f39088e2787fe977306ce2
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2022-05-01 11:28:45 +0200

    Corrected Test for method drawStudents in BagTest

--- src/test/java/it/polimi/ingsw/model/BagTest.java
+++ src/test/java/it/polimi/ingsw/model/BagTest.java
@@ -11,12 +11,12 @@ import java.util.Arrays;
  * @author Sebastiano Meneghin
  */
 class BagTest {
-    private Bag bagTest;
-    private Field studentCountersField;
-    private final int[] studentCountersTest = {1,2,3,4,5};
+    private       Bag   bagTest;
+    private       Field studentCountersField;
+    private final int[] studentCountersTest           = {1,2,3,4,5};
     private final int[] studentCountersTestSingleDraw = {1,0,0,0,0};
     private final int[] studentCountersTestDoubleDraw = {1,1,0,0,0};
-
+    private       Field numOfStudentsField;
 
     @BeforeEach
     void setUp() throws IllegalAccessException, NoSuchFieldException {
@@ -26,7 +26,12 @@ class BagTest {
         studentCountersField = bagTest.getClass().getDeclaredField("studentCounters");
         studentCountersField.setAccessible(true);
 
+        // Use reflection to get the private field "numOfStudent" and change it visibility
+        numOfStudentsField = bagTest.getClass().getDeclaredField("numOfStudents");
+        numOfStudentsField.setAccessible(true);
+
         studentCountersField.set(bagTest, new int[Color.values().length]);
+        numOfStudentsField.set(bagTest, 0);
     }
 
     /**
@@ -36,9 +41,10 @@ class BagTest {
     void getStudentCountersTest() throws IllegalAccessException {
         studentCountersField.set(bagTest, Arrays.copyOf(studentCountersTest, studentCountersTest.length));
 
-        for (Color color : Color.values())
+        for (Color color : Color.values()) {
             if (studentCountersTest[color.ordinal()] != bagTest.getStudentCounters(color))
                 throw new AssertionError("Getter returned wrong value");
+        }
     }
 
     /**
@@ -46,17 +52,29 @@ class BagTest {
      */
     @Test
     void setStudentCountersTest() throws IllegalAccessException {
+        // Local variable used for checking the numOfStudents' update procedure
+        int numOfAddedStudents = 0;
+
         // Sets all counters independently and tries to retrieve the same value
         for (Color color : Color.values()) {
             bagTest.setStudentCounters(color, studentCountersTest[color.ordinal()]);
+            numOfAddedStudents = numOfAddedStudents + studentCountersTest[color.ordinal()];
+
             if (studentCountersTest[color.ordinal()] != ((int[]) studentCountersField.get(bagTest))[color.ordinal()])
                 throw new AssertionError("Independent setter set wrong value");
         }
 
+        if ((int) numOfStudentsField.get(bagTest) != numOfAddedStudents)
+            throw new AssertionError("Setter did not update correctly the counter 'numOfStudents' ");
+
+
         // Sets all counters to the first value of the test set and tries to retrieve all the values
+        numOfAddedStudents = 0;
         bagTest.setStudentCounters(studentCountersTest[0]);
         int[] studentCountersTest = (int[]) studentCountersField.get(bagTest);
         for (Color color : Color.values()) {
+            numOfAddedStudents = numOfAddedStudents + studentCountersTest[color.ordinal()];
+
             if (studentCountersTest[color.ordinal()] != studentCountersTest[0])
                 throw new AssertionError("Global setter set wrong value");
         }
@@ -66,34 +84,40 @@ class BagTest {
     /**
      * Test for both methods drawStudents of class "Bag"
      */
-    /*
     @Test
     void drawStudentsTest() throws IllegalAccessException {
+        BagResult drawResult;
+
         //Set a single counter to 1 and try a single draw
         studentCountersField.set(bagTest, Arrays.copyOf(studentCountersTestSingleDraw, studentCountersTestSingleDraw.length));
+        numOfStudentsField.set(bagTest, studentCountersTestSingleDraw.length);
 
         try {
-            if (bagTest.drawStudents() != Color.values()[0])
+            drawResult = bagTest.drawStudents(1);
+            if (drawResult.drawnStudents()[0] != Color.values()[0])
                 throw new AssertionError("Single drawStudents drawn wrong student");
+
+            if (drawResult.drawnStudents().length != 1)
+                throw new AssertionError("Single drawStudents drawn more than one student");
         }
         catch (EmptyBagException e) {
-            throw new AssertionError("Single drawStudents emptied the bag without drawing a student");
+            throw new AssertionError("Test has been implemented incorrectly");
         }
 
 
         //Set two different counter to 1 and try a double draw
         studentCountersField.set(bagTest, Arrays.copyOf(studentCountersTestDoubleDraw, studentCountersTestDoubleDraw.length));
-        Color[] colorDoubleTest;
+        numOfStudentsField.set(bagTest, studentCountersTestDoubleDraw.length);
 
         try {
-            colorDoubleTest = bagTest.drawStudents(2);
+            drawResult = bagTest.drawStudents(2);
         }
         catch (EmptyBagException e) {
-            throw new AssertionError("Double drawStudents emptied the bag without drawing both of the students");
+            throw new AssertionError("Test has been implemented incorrectly");
         }
 
-        if ((colorDoubleTest[0] != Color.RED && colorDoubleTest[1] != Color.RED) || (colorDoubleTest[0] != Color.YELLOW && colorDoubleTest[1] != Color.YELLOW))
+        if ((drawResult.drawnStudents()[0] != Color.RED && drawResult.drawnStudents()[1] != Color.RED) || (drawResult.drawnStudents()[0] != Color.YELLOW && drawResult.drawnStudents()[1] != Color.YELLOW))
             throw new AssertionError("Double drawStudents drawn wrong students");
     }
-    */
+
 }
