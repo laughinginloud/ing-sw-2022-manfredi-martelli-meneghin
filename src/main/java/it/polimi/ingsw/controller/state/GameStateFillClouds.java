95d8e90f8d6ddf0b3b5dcbebade590d194864aab
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2022-05-07 01:18:34 +0200

    Ended GameStateFillClouds game-logic part, Added GameStatePlayCard game-logic part

--- src/main/java/it/polimi/ingsw/controller/state/GameStateFillClouds.java
+++ src/main/java/it/polimi/ingsw/controller/state/GameStateFillClouds.java
@@ -1,17 +1,46 @@
 package it.polimi.ingsw.controller.state;
 
+import it.polimi.ingsw.controller.ControllerData;
+import it.polimi.ingsw.model.*;
+
 /**
  * Class containing the cloud filling procedure of the bigger PlanPhase
  * @author Sebastiano Meneghin
  */
 public class GameStateFillClouds implements GameStatePlanPhase {
     public GameState nextState() {
-        //return new GameStatePlayCard();
-        return null;
+        return new GameStatePlayCard();
     }
 
+    @Override
     public void executeState() {
-        //TODO: Fill Clouds
-        //TODO: Activate emptyBagTrigger if number of returned students isn't enough to fill all the clouds
+        // There's one CloudTile for each player, it is required to fill all of them
+        int numOfClouds = ControllerData.getInstance().getNumOfPlayers();
+        for (int i = 0; i <numOfClouds; i++) {
+            fillCloud(ControllerData.getInstance().getGameModel().getCloudTile(i));
+        }
+
+        //TODO: [Command] notify all the Players about the filled clouds
+    }
+
+    /**
+     * Set a precise number of student on a CloudTile, depending on the number of the player. If it empties the bag it sets "true" the EmptyBagTrigger
+     * @param cloudTile The cloudTile to be filled of students
+     */
+    private void fillCloud (CloudTile cloudTile) {
+        int studentsToDraw;
+
+        switch (ControllerData.getInstance().getNumOfPlayers()) {
+            case 2, 4 -> studentsToDraw = 3;
+            case 3    -> studentsToDraw = 4;
+            default   -> throw new IllegalStateException("The number of students to set on a CloudTile could be only 3 or 4");
+        }
+
+        try {
+            cloudTile.setStudents(ControllerData.getInstance().getGameModel().getBag().drawStudents(studentsToDraw).drawnStudents());
+        }
+        catch (EmptyBagException e){
+            ControllerData.getInstance().setEmptyBagTrigger();
+        }
     }
 }
---
+++ src/main/java/it/polimi/ingsw/controller/state/GameStatePlayCard.java
@@ -0,0 +1,110 @@
+package it.polimi.ingsw.controller.state;
+
+import it.polimi.ingsw.controller.ControllerData;
+import it.polimi.ingsw.model.*;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class controlling the PlayCardPhase of the PlanPhase of every round
+ * @author Sebastiano Meneghin
+ */
+public class GameStatePlayCard implements GameStatePlanPhase {
+    public GameState nextState() {
+        // return new GameStateSelectTurnOrder();
+        return null;
+    }
+
+    @Override
+    public void executeState() {
+        // Clean the PlayerAssistantCardMap, removing previous rounds' played cards
+        ControllerData.getInstance().nukePlayerAssistantCardMap();
+        Player[] players = ControllerData.getInstance().getPlayersOrder();
+
+        for (Player player : players) {
+            ControllerData.getInstance().setCurrentPlayer(player);
+            AssistantCard[] playableAssistantCards = getPlayableAssistantCards(player);
+
+
+            //TODO: [Command] request an action to the current player, sending him the playableAC! (He has to choose between one of the provided AssistantCards
+            //PLACE-HOLDER!
+            AssistantCard chosenCard = new AssistantCard(1,1);
+
+            saveAssistantCardChoice(player, chosenCard);
+
+            if (player.getAssistantDeck().length == 0)
+                ControllerData.getInstance().setEmptyAssistantDeckTrigger();
+        }
+    }
+
+
+    /**
+     * Get all the playable AssistantCards from a specific player's deck, according to the Eriantys Rules
+     * @param player The current player from whose deck we need to draw the playable AssistantCards
+     * @return An array of AssistantCards containing the playable AssistantCards
+     */
+    private AssistantCard[] getPlayableAssistantCards (Player player) {
+        AssistantCard[] availableCards = player.getAssistantDeck();
+        List<AssistantCard> playableCards = new LinkedList<>();
+        Map<Player, AssistantCard> alreadyPlayedCards = ControllerData.getInstance().getPlayerAssistantCardMap();
+
+        if (availableCards.length == 0)
+            throw new IllegalStateException("The FSM cannot return to the PlanPhase if the deck is empty");
+
+        // If there are already played AssistantCards, check which AssistantCards can be played by the current player
+        if (!alreadyPlayedCards.isEmpty()) {
+
+            // Find the AssistantCards in the current player deck which hasn't already been used by other players during this turn
+            for (AssistantCard assistantCard : availableCards) {
+                if (!alreadyPlayedCards.containsValue(assistantCard))
+                    playableCards.add(assistantCard);
+            }
+
+            // If playableCards is empty, the current player has only already played cards in his deck, so he can play them
+            if (playableCards.isEmpty())
+                return availableCards;
+
+
+            AssistantCard[] remainingAssistantCards = new AssistantCard[playableCards.size()];
+            for (int i = 0; i < playableCards.size(); i++)
+                remainingAssistantCards[i] = playableCards.get(i);
+
+            return remainingAssistantCards;
+        }
+
+        return availableCards;
+    }
+
+    /**
+     * Does everything is needed when a player draws an AssistantCard from his deck
+     * @param player The Player who just chose an AssistantCard
+     * @param chosenCard The AssistantCard that has been chosen
+     */
+    private void saveAssistantCardChoice(Player player, AssistantCard chosenCard) {
+        // Gets the AssistantCard from the current player's deck
+        AssistantCard temp = player.getAssistantCard(chosenCard.cardValue());
+        if (!temp.equals(chosenCard))
+            throw new IllegalStateException("The two AssistantCards cannot be different");
+
+        // Save the usage of the chosenCard into the ControllerData's PlayerAssistantCardMap
+        ControllerData.getInstance().addPlayerCard(player, chosenCard);
+
+        // Set the chosenCard as lastPlayerCard on the current player board
+        player.setLastPlayedCard(chosenCard);
+
+
+        //TODO: [Command] notify all the players about the lastPlayerCard now on the board of the current player
+
+    }
+
+}
+
+
+
+
+
+
+
+
