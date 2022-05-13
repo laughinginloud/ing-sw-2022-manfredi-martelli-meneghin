7aa7794eaf0a724d1bb760f6b64b4d8c32984b4a
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2022-05-13 04:14:55 +0200

    Added use of RequestAction to GameStateChooseCloud, added characterCard Selection, modified endOfState condition

--- src/main/java/it/polimi/ingsw/controller/state/GameStateChooseCloud.java
+++ src/main/java/it/polimi/ingsw/controller/state/GameStateChooseCloud.java
@@ -2,12 +2,12 @@ package it.polimi.ingsw.controller.state;
 
 import it.polimi.ingsw.controller.ControllerData;
 import it.polimi.ingsw.controller.command.*;
-import it.polimi.ingsw.model.Character;
-import it.polimi.ingsw.model.Color;
-import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.*;
 import it.polimi.ingsw.virtualView.VirtualView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,8 +21,39 @@ public class GameStateChooseCloud implements GameStateActionPhase {
         try {
             Player player          = ControllerData.getInstance().getCurrentPlayer();
             VirtualView playerView = ControllerData.getInstance().getPlayerView(player);
+            Map<GameCommandValues, Object> chooseCloudInfo = new HashMap<>();
+            boolean expertMode = ControllerData.getInstance().getExpertMode();
+            boolean canPlayCharacterCard = false;
 
-            GameCommand request    = new GameCommandRequestValueClient(GameCommandValues.CLOUD);
+            // Gets all the cloud still filled by students
+            CloudTile[] availableCloud = getAvailableClouds();
+            chooseCloudInfo.put(GameCommandValues.CLOUDARRAY, availableCloud);
+
+            // If there are not cloudTile filled by at least one student, end GameStateChooseCloud
+            if (availableCloud == null)
+                return;
+
+            // If the player hasn't player a card yet
+            if (expertMode && !ControllerData.getInstance().checkPlayedCard()) {
+                // Get all the playableCharacterCard, according to previous CharacterCards utilization and to current player's coin pool
+                /* TODO: [CharacterCardChecking] Link to the getPlayableCharacterFunction
+                   CharacterCard[] playableCharacterCard = getPlayableCharacterCard(player); */
+                CharacterCard[] playableCharacterCard = null; //TO CHANGE!
+
+                // If there are CharacterCard playable by the current player, adds them to the moveStudentsInfo
+                if (playableCharacterCard != null) {
+                    chooseCloudInfo.put(GameCommandValues.CHARACTERCARDARRAY, playableCharacterCard);
+
+                    //Set the flag to true to change the GameCommandRequestAction's type during its initialization
+                    canPlayCharacterCard = true;
+                }
+            }
+
+            // Send a RequestAction to the current player, asking him to choose a CloudTile between the provided ones
+            // If the player is allowed to, send also the CharacterCard he could play
+            GameCommand request    = canPlayCharacterCard ?
+                new GameCommandRequestAction(GameCommandActions.CHOOSECLOUDORPLAYCARD, chooseCloudInfo):
+                new GameCommandRequestAction(GameCommandActions.CHOOSECLOUD, chooseCloudInfo);
             GameCommand response   = playerView.sendRequest(request);
 
             // If the response is of the right type, try to execute
@@ -35,6 +66,7 @@ public class GameStateChooseCloud implements GameStateActionPhase {
 
                     Map<GameCommandValues, Object> map = new HashMap<>();
                     map.put(GameCommandValues.ENTRANCE, player.getSchoolBoard().getEntrance());
+                    map.put(GameCommandValues.CLOUDARRAY, ControllerData.getInstance().getGameModel().getCloudTile());
 
                     for (Player playerToUpdate : ControllerData.getInstance().getPlayersOrder())
                         ControllerData.getInstance().getPlayerView(playerToUpdate).sendMessage(new GameCommandSendInfo(map));
@@ -61,7 +93,7 @@ public class GameStateChooseCloud implements GameStateActionPhase {
                     throw new IllegalStateException("CharacterCard has been already used by the current player!");
 
                 // Executes the command received and set to "true" the flag hasPlayedCard stored in ControllerData
-                Character calledCharacter = (Character) c.executeCommand();
+                CharacterCard calledCharacter = (CharacterCard) c.executeCommand();
                 ControllerData.getInstance().setPlayedCard();
 
                 /* TODO: [CharacterCard] Insert here the real name of the function which manage the CharacterCardUse */
@@ -89,6 +121,30 @@ public class GameStateChooseCloud implements GameStateActionPhase {
         catch (Exception e) {
             // Fatal error: print the stack trace to help debug
             e.printStackTrace();
+        }
+    }
+
+    private CloudTile[] getAvailableClouds() {
+        CloudTile[] presentCloudTiles = ControllerData.getInstance().getGameModel().getCloudTile();
+        CloudTile[] cloudTilesToSend;
+        List<CloudTile> availableCloudTiles = new ArrayList<>();
+
+        for (CloudTile cloudTile : presentCloudTiles)
+            for (Color color : cloudTile.getStudents())
+                if (color != null)
+                    availableCloudTiles.add(cloudTile);
+
+        // If there are no available CloudTiles
+        if (availableCloudTiles.isEmpty())
+            return null;
+
+        // If there is at least one available CloudTile
+        else {
+            cloudTilesToSend = new CloudTile[availableCloudTiles.size()];
+            for (int i = 0; i < availableCloudTiles.size(); i++)
+                cloudTilesToSend[i] = availableCloudTiles.get(i);
+
+            return cloudTilesToSend;
         }
     }
 }
