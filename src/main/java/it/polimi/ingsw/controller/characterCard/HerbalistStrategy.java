accd3908d734699c6ceb54a07b1c37604830b59e
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2022-05-14 17:42:58 +0200

    Added gameCommand logic to HerbalistStrategy

--- src/main/java/it/polimi/ingsw/controller/characterCard/HerbalistStrategy.java
+++ src/main/java/it/polimi/ingsw/controller/characterCard/HerbalistStrategy.java
@@ -1,6 +1,15 @@
 package it.polimi.ingsw.controller.characterCard;
 
+import it.polimi.ingsw.controller.ControllerData;
+import it.polimi.ingsw.controller.command.*;
 import it.polimi.ingsw.model.CharacterCard;
+import it.polimi.ingsw.model.GameModel;
+import it.polimi.ingsw.model.Island;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.virtualView.VirtualView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Strategy representing the activation of the CharacterCard 'HERBALIST'
@@ -16,13 +25,72 @@ public class HerbalistStrategy extends CharacterCardStrategy {
      */
     @Override
     public void activateEffect() {
-        // TODO [CharacterCardStrategy] implementation
-        // The player sends the index of the chosen Card to play
-        // If there are still noEntryTiles then [...] Else the effect does nothing.
-        // The server asks the player on which island he wants to place the noEntryTile (requestAction())
-        // The player responds with the information requested by the server (responseAction(island))
-        // The server places the token on the the selected island (noEntryTile++)
-        // The server sets the Player to hasPlayedCard = true
-        // sendInfo to all players
+        try {
+            ControllerData data       = ControllerData.getInstance();
+            GameModel      model      = data.getGameModel();
+            Player         curPlayer  = data.getCurrentPlayer();
+            VirtualView    playerView = data.getPlayerView(curPlayer);
+
+            // The server gets the Island where a noEntryTile could be placed using the characterCard effect
+            Island[] availableIslands = model.getIslands();
+
+            // Create a Map and save the field that will be sent to the player as RequestAction's payload
+            Map<GameCommandValues, Object> herbalistMap = new HashMap<>();
+            herbalistMap.put(GameCommandValues.ISLANDARRAY, availableIslands);
+
+            // The server asks the player on which Island he would like to put the noEntryTile
+            GameCommand request = new GameCommandRequestAction(GameCommandActions.CHARACTERCARDEFFECT, herbalistMap);
+            GameCommand response = playerView.sendRequest(request);
+
+            // If the response is of the right kind
+            if (response instanceof GameCommandChosenCharacterCardFields c) {
+                // The player responds with the information requested by the server
+                @SuppressWarnings("unchecked")
+                Map<GameCommandValues, Object> chosenField = (Map<GameCommandValues, Object>) c.executeCommand();
+
+                // Gets the value of the chosenField from the Map
+                Island chosenIsland = chosenField.get(GameCommandValues.ISLANDINDEX);
+
+                // TODO [CharacterCardStrategy] implementation
+                // The server places the token on the the selected island (noEntryTile++)
+                // sendInfo to all players
+
+
+                // After the server managed the use of the CharacterCard, gets the updated values of CharacterCardsArray and IslandsArray
+                CharacterCard[] updatedCharacterCards = model.getCharacterCards();
+                Island[]        updatedIslands        = model.getIslands();
+
+                // Creates a map containing the updated field to send to all the players
+                Map<GameCommandValues, Object> afterEffectUpdate = new HashMap<>();
+                afterEffectUpdate.put(GameCommandValues.CHARACTERCARDARRAY, updatedCharacterCards);
+                afterEffectUpdate.put(GameCommandValues.ISLANDARRAY, updatedIslands);
+
+                // Sends to all the players the updated fields
+                Player[] players = data.getPlayersOrder();
+                for (Player playersToUpdate : players) {
+                    VirtualView playerToUpdateView = data.getPlayerView(playersToUpdate);
+                    playerToUpdateView.sendMessage(new GameCommandSendInfo(afterEffectUpdate));
+                }
+            }
+
+            // If the response is of the wrong kind, send an Illegal Command message and restart the method
+            else {
+                try {
+                    playerView.sendMessage(new GameCommandIllegalCommand());
+                }
+
+                catch (Exception ex) {
+                    // Fatal error: print the stack trace to help debug
+                    ex.printStackTrace();
+                }
+
+                activateEffect();
+            }
+        }
+
+        catch (Exception e){
+            // Fatal error: print the stack trace to help debug
+            e.printStackTrace();
+        }
     }
 }
