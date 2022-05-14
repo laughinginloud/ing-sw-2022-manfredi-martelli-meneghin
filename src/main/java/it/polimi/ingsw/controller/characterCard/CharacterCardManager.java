1cff66e16ca5fef9c064403206d8083180fa2081
Mattia Martelli <mattia.martelli@live.it>
2022-05-14 21:45:20 +0200

    Changed the if chain in CharacterCardManager into a switch

--- src/main/java/it/polimi/ingsw/controller/characterCard/CharacterCardManager.java
+++ src/main/java/it/polimi/ingsw/controller/characterCard/CharacterCardManager.java
@@ -52,61 +52,60 @@ public class CharacterCardManager {
         Character character = characterCard.getCharacter();
         boolean   characterCardPlayability = false;
 
-        // MONK, BARD: Check if there is at least one student still on the CharacterCard, that can be then moved
-        if (character == Character.MONK || character == Character.PRINCESS) {
-            CharacterCardStudent characterCardStudent = (CharacterCardStudent) characterCard;
-            Color[] studentColors = characterCardStudent.getStudents();
-
-            for (Color studentColor : studentColors)
-                if (studentColor != null) {
-                    characterCardPlayability = true;
-                    break;
-                }
-        }
-
-        // FARMER: Check if there is at least on Professor which has been moved to a player's DiningRoom
-        else if (character == Character.FARMER) {
-            GlobalProfessorTable gpt = ControllerData.getInstance().getGameModel().getGlobalProfessorTable();
-
-            for (Color color : Color.values())
-                if (gpt.getProfessorLocation(color) != null)
-                    characterCardPlayability = true;
-        }
-
-        // HERBALIST: Check if there is at least on noEntryTile still on the card
-        else if (character == Character.HERBALIST) {
-            CharacterCardNoEntry characterCardNoEntry = (CharacterCardNoEntry) characterCard;
-            if (characterCardNoEntry.getNoEntryCount() > 0)
-                characterCardPlayability = true;
-        }
-
-        // BARD: Check if there is at least one student in the current player DiningRoom
-        else if (character == Character.BARD) {
-            DiningRoom currentPlayerDiningRoom = player.getSchoolBoard().getDiningRoom();
-            for (Color color : Color.values())
-                if (currentPlayerDiningRoom.getStudentCounters(color) > 0)
-                    characterCardPlayability = true;
-        }
-
-        // THIEF: Check if there is at least one student in one of all players' DiningRoom
-        else if (character == Character.THIEF) {
-            Player[]     players     = ControllerData.getInstance().getPlayersOrder();
-            DiningRoom[] diningRooms = new DiningRoom[players.length];
-
-            // Gets the players' diningRoom
-            for (int i = 0; i < players.length; i++)
-                diningRooms[i] = players[i].getSchoolBoard().getDiningRoom();
-
-            // Check every diningRoom and set characterCardPlayability to true if a student is found
-            for (DiningRoom diningRoom : diningRooms)
-                for (Color color : Color.values())
-                    if (diningRoom.getStudentCounters(color) > 0)
+        switch (character) {
+            // MONK, BARD: Check if there is at least one student still on the CharacterCard, that can be then moved
+            case MONK, PRINCESS -> {
+                CharacterCardStudent characterCardStudent = (CharacterCardStudent) characterCard;
+                Color[] studentColors = characterCardStudent.getStudents();
+                for (Color studentColor : studentColors)
+                    if (studentColor != null) {
                         characterCardPlayability = true;
-        }
+                        break;
+                    }
+            }
 
-        // If this characterCar's utilization is always allowed
-        else
-            characterCardPlayability = true;
+            // FARMER: Check if there is at least on Professor which has been moved to a player's DiningRoom
+            case FARMER -> {
+                GlobalProfessorTable gpt = ControllerData.getInstance().getGameModel().getGlobalProfessorTable();
+                for (Color color : Color.values())
+                    if (gpt.getProfessorLocation(color) != null)
+                        characterCardPlayability = true;
+            }
+
+            // HERBALIST: Check if there is at least on noEntryTile still on the card
+            case HERBALIST -> {
+                CharacterCardNoEntry characterCardNoEntry = (CharacterCardNoEntry) characterCard;
+                if (characterCardNoEntry.getNoEntryCount() > 0)
+                    characterCardPlayability = true;
+            }
+
+            // BARD: Check if there is at least one student in the current player DiningRoom
+            case BARD -> {
+                DiningRoom currentPlayerDiningRoom = player.getSchoolBoard().getDiningRoom();
+                for (Color color : Color.values())
+                    if (currentPlayerDiningRoom.getStudentCounters(color) > 0)
+                        characterCardPlayability = true;
+            }
+
+            // THIEF: Check if there is at least one student in one of all players' DiningRoom
+            case THIEF -> {
+                Player[] players = ControllerData.getInstance().getPlayersOrder();
+                DiningRoom[] diningRooms = new DiningRoom[players.length];
+
+                // Gets the players' diningRoom
+                for (int i = 0; i < players.length; i++)
+                    diningRooms[i] = players[i].getSchoolBoard().getDiningRoom();
+
+                // Check every diningRoom and set characterCardPlayability to true if a student is found
+                for (DiningRoom diningRoom : diningRooms)
+                    for (Color color : Color.values())
+                        if (diningRoom.getStudentCounters(color) > 0)
+                            characterCardPlayability = true;
+            }
+
+            // If this characterCar's utilization is always allowed
+            default -> characterCardPlayability = true;
+        }
 
         return characterCardPlayability;
     }
