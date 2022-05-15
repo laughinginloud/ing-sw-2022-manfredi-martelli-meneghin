3a08a2a9e8182f217fd18e0c812252d1b3e3c205
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-15 10:01:33 +0200

    Modified CavalierStrategy  and Added TODO confirm message

--- src/main/java/it/polimi/ingsw/controller/characterCard/CavalierStrategy.java
+++ src/main/java/it/polimi/ingsw/controller/characterCard/CavalierStrategy.java
@@ -9,6 +9,11 @@ import it.polimi.ingsw.model.GameModel;
  * @author Giovanni Manfredi
  */
 public class CavalierStrategy extends CharacterCardStrategy {
+
+    /**
+     * Constructor of the class 'CavalierStrategy'
+     * @param card the card to which the class is initialized
+     */
     public CavalierStrategy(CharacterCard card) {
         this.card = card;
     }
@@ -19,11 +24,10 @@ public class CavalierStrategy extends CharacterCardStrategy {
     @Override
     public void activateEffect() {
         ControllerData data = ControllerData.getInstance();
-        GameModel model = data.getGameModel();
 
-        // TODO [CharacterCardStrategy] @Seba command implementation
         // The server increases the influence of the current player of two by setting the flag: extraInfluenceFlag
         data.setCharacterCardFlag(ControllerData.Flags.extraInfluenceFlag, true);
-        // sendInfo to all players
+
+        // TODO [CharacterCardStrategy]: Command implementation - Confirm Message operation successful
     }
 }
