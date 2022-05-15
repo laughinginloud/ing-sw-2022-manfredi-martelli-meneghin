f5cc7e10093cf83583951839006f9347a9482185
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-15 10:02:38 +0200

    Modified CentaurStrategy and Added TODO confirm message

--- src/main/java/it/polimi/ingsw/controller/characterCard/CentaurStrategy.java
+++ src/main/java/it/polimi/ingsw/controller/characterCard/CentaurStrategy.java
@@ -9,6 +9,11 @@ import it.polimi.ingsw.model.GameModel;
  * @author Giovanni Manfredi
  */
 public class CentaurStrategy extends CharacterCardStrategy {
+
+    /**
+     * Constructor of the class 'CentaurStrategy'
+     * @param card the card to which the class is initialized
+     */
     public CentaurStrategy(CharacterCard card) {
         this.card = card;
     }
@@ -19,11 +24,10 @@ public class CentaurStrategy extends CharacterCardStrategy {
     @Override
     public void activateEffect() {
         ControllerData data = ControllerData.getInstance();
-        GameModel model = data.getGameModel();
-
-        // TODO [CharacterCardStrategy]: @Seba Command implementation
+        
         // The server "deactivates" the towers which are not counted for the influence - sets the flag ignoreTowersFlag
         data.setCharacterCardFlag(ControllerData.Flags.ignoreTowersFlag, true);
-        // sendInfo to all players
+
+        // TODO [CharacterCardStrategy]: Command implementation - Confirm Message operation successful
     }
 }
