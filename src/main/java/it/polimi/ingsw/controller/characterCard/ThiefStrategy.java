fa8e668f962de9f5d4c83d0b5aa366d991940180
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-15 10:05:25 +0200

    Implemented ThiefStrategy Logic and Added TODO commands

--- src/main/java/it/polimi/ingsw/controller/characterCard/ThiefStrategy.java
+++ src/main/java/it/polimi/ingsw/controller/characterCard/ThiefStrategy.java
@@ -1,12 +1,20 @@
 package it.polimi.ingsw.controller.characterCard;
 
+import it.polimi.ingsw.controller.ControllerData;
 import it.polimi.ingsw.model.CharacterCard;
+import it.polimi.ingsw.model.GameModel;
+import it.polimi.ingsw.model.Player;
 
 /**
  * Strategy representing the activation of the CharacterCard 'THIEF'
  * @author Giovanni Manfredi
  */
 public class ThiefStrategy extends CharacterCardStrategy {
+
+    /**
+     * Constructor of the class 'ThiefStrategy'
+     * @param card the card to which the class is initialized
+     */
     public ThiefStrategy(CharacterCard card) {
         this.card = card;
     }
@@ -16,10 +24,28 @@ public class ThiefStrategy extends CharacterCardStrategy {
      */
     @Override
     public void activateEffect() {
-        // TODO [CharacterCardStrategy] @Gio @Seba implementation
+        ControllerData data = ControllerData.getInstance();
+        GameModel model = data.getGameModel();
+
+        // TODO [CharacterCardStrategy] Command implementation
         // The server asks the player which color he wants to reduce from each player (requestAction())
-        // The player responds with the information requested by the server (responseAction(color))
+        // The player responds with the information requested by the server (responseAction(removeStudents))
+
         // The server removes 3 (or all) students of the specified color from each SchoolBoard
+        Player[] players = model.getPlayer();
+        int currentStudents;
+
+        for (Player player: players) {
+            currentStudents = player.getSchoolBoard().getDiningRoom().getStudentCounters(removeStudents);
+            if (currentStudents >= 3) {
+                player.getSchoolBoard().getDiningRoom().setStudentCounters(removeStudents, currentStudents - 3);
+            }
+            else {
+                player.getSchoolBoard().getDiningRoom().setStudentCounters(removeStudents, 0);
+            }
+        }
+
+        // TODO [CharacterCardStrategy] Command implementation
         // sendInfo to all players
     }
 }
