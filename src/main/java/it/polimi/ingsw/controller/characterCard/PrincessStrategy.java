f7fbfa697dedd3eb9e1efb1ed50146800eae5554
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-15 10:04:56 +0200

    Implemented PrincessStrategy Logic and Added TODO commands

--- src/main/java/it/polimi/ingsw/controller/characterCard/PrincessStrategy.java
+++ src/main/java/it/polimi/ingsw/controller/characterCard/PrincessStrategy.java
@@ -1,12 +1,18 @@
 package it.polimi.ingsw.controller.characterCard;
 
-import it.polimi.ingsw.model.CharacterCard;
+import it.polimi.ingsw.controller.ControllerData;
+import it.polimi.ingsw.model.*;
 
 /**
  * Strategy representing the activation of the CharacterCard 'PRINCESS'
  * @author Giovanni Manfredi
  */
 public class PrincessStrategy extends CharacterCardStrategy {
+
+    /**
+     * Constructor of the class 'PrincessStrategy'
+     * @param card the card to which the class is initialized
+     */
     public PrincessStrategy(CharacterCard card) {
         this.card = card;
     }
@@ -16,11 +22,32 @@ public class PrincessStrategy extends CharacterCardStrategy {
      */
     @Override
     public void activateEffect() {
-        // TODO [CharacterCardStrategy] @Gio @Seba implementation
-        // The server asks the player which student he wants to move and the schoolBoard he wants to move the player to (requestAction())
-        // The player responds with the information requested by the server (responseAction(student))
-        // The server moves the student from the CharacterCard to the SchoolBoard (FullBoardException)
+        ControllerData data = ControllerData.getInstance();
+        GameModel model = data.getGameModel();
+        Player currentPlayer = data.getCurrentPlayer();
+
+        // TODO [CharacterCardStrategy] Command implementation
+        // The server asks the player which student he wants to move to a DiningRoom (requestAction())
+        // The player responds with the information requested by the server (responseAction(student_index))
+
+        // The server moves the student from the CharacterCard to the SchoolBoard (FullBoardException?)
+        Color movedStudent = ((CharacterCardStudent) card).retrieveStudent(student_index);
+        DiningRoom diningRoom = currentPlayer.getSchoolBoard().getDiningRoom();
+        int currentStudents = diningRoom.getStudentCounters(movedStudent);
+        diningRoom.setStudentCounters(movedStudent, currentStudents + 1);
+
         // The server refills the card taking a student from the Bag (EmptyBagException)
+        try {
+            movedStudent = model.getBag().drawStudents(1).drawnStudents()[0];
+        }
+
+        catch (EmptyBagException e){
+            data.setEmptyBagTrigger();
+        }
+
+        ((CharacterCardStudent) card).appendStudent(movedStudent);
+
+        // TODO [CharacterCardStrategy] Command implementation
         // sendInfo to all players
     }
 }
