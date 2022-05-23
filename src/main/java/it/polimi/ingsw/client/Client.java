c71daa9a6dd0ab68dd3fb63b39c566d90d093763
Mattia Martelli <mattia.martelli@live.it>
2022-05-23 19:10:59 +0200

    Temporary compilation fix to the client

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.client.view.*;
 import it.polimi.ingsw.client.view.cli.ViewCLI;
 import it.polimi.ingsw.client.view.gui.ViewGUI;
 import it.polimi.ingsw.client.virtualController.VirtualController;
+import it.polimi.ingsw.common.model.GameModel;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -12,21 +13,46 @@ import java.net.Socket;
 import java.net.SocketException;
 
 public class Client {
-    public static void main(String[] args) {
-        View view = args.length != 0 && args[0].equals("console") ? new ViewCLI() : new ViewGUI();
+    private static GameModel model;
 
+    public static void main(String[] args) {
+        /*
+        View view = args.length != 0 && args[0].equals("console") ? new ViewCLI() : new ViewGUI();
         view.initialize();
 
-        try (VirtualController controller = new VirtualController(view.getAddress())) {
-            //TODO
-        }
+        boolean breakLoop = false;
 
-        catch (SocketException ignored) {
-            view.signalConnectionError();
+        do {
+            switch (view.menu()) {
+                case Connect -> startGame(view);
+                case Exit    -> breakLoop = true;
+            }
+        } while (!breakLoop);
+    }
+
+    public static void setModel(GameModel model) {
+        Client.model = model;
+    }
+
+    public static GameModel getModel() {
+        return model;
+    }
+
+    private static void startGame(View view) {
+        try (VirtualController controller = new VirtualController(view.getAddress(), view)) {
+            controller.start();
+            controller.join();
         }
 
         catch (IOException e) {
-            throw new RuntimeException(e);
+            view.signalConnectionError(); //FIXME: signalCannotConnect();
         }
+
+        catch (Exception e) {
+
+        }
+
+        //View signal end
+         */
     }
 }
--- src/main/java/it/polimi/ingsw/client/view/cli/ViewCLI.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/ViewCLI.java
@@ -2,11 +2,42 @@ package it.polimi.ingsw.client.view.cli;
 
 import it.polimi.ingsw.client.Address;
 import it.polimi.ingsw.client.view.View;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
-public class ViewCLI implements View {
+import java.io.PrintWriter;
+
+public abstract class ViewCLI implements View {
+    Terminal terminal;
+
+    PrintWriter writer;
 
     public void initialize() {
+        try {
+            terminal = TerminalBuilder.builder()
+                .name("Eriantys")
+                .encoding("UTF-8")
+                .nativeSignals(true)
+                .type("ansi")
+                .jna(true)
+                .build();
 
+            writer = terminal.writer();
+            writer.println(
+                """
+                 _____      _             _
+                | ____|_ __(_) __ _ _ __ | |_ _   _ ___
+                |  _| | '__| |/ _` | '_ \\| __| | | / __|
+                | |___| |  | | (_| | | | | |_| |_| \\__ \\
+                |_____|_|  |_|\\__,_|_| |_|\\__|\\__, |___/
+                                              |___/
+                """);
+            writer.flush();
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void setUpBoard() {
