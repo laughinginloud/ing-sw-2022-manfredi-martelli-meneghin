40728a6e970942c9449b036d2e7a4786040db94a
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-26 14:35:16 +0200

    Added UnableToJoinHandler and linked to unableToJoinPage.fxml

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/UnableToJoinHandler.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.client.view.gui.sceneHandlers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class UnableToJoinHandler {
+
+    @FXML
+    private AnchorPane unableToJoin_pane;
+
+    @FXML
+    private ImageView unableToJoin_imgView;
+
+    @FXML
+    private Label unableToJoin_title;
+
+    @FXML
+    private Label unableToJoin_text;
+}
