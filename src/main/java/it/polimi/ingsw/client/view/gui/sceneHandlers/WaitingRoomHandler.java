f6f69b19b97c3a45b27c4a87fe79a722b314a900
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-26 14:36:05 +0200

    Added WaitingRoomHandler and linked to waitingRoomPage.fxml

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/WaitingRoomHandler.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.client.view.gui.sceneHandlers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class WaitingRoomHandler {
+
+    @FXML
+    private AnchorPane waitingRoom_pane;
+
+    @FXML
+    private ImageView waitingRoom_imgView;
+
+    @FXML
+    private Label waitingRoom_title;
+
+    @FXML
+    private Label waitingRoom_text;
+
+}
