e1fca86d30a11665a52aa4a7adb3298796d2cce4
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-26 14:33:43 +0200

    Added GameChoiceHandler and linked to gameChoicePage.fxml

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/GameChoiceHandler.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.client.view.gui.sceneHandlers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class GameChoiceHandler {
+
+    @FXML
+    private AnchorPane gameChoice_pane;
+
+    @FXML
+    private ImageView gameChoice_imgView;
+
+    @FXML
+    private Button continue_button;
+
+    @FXML
+    private Button newGame_button;
+}
