f6b7eb863f524d6415c32ed38a73ecea59ab6e56
Mattia Martelli <mattia.martelli@live.it>
2022-05-27 18:58:27 +0200

    Moved TermColor to common.termutils, added Key

---
+++ src/main/java/it/polimi/ingsw/common/termutils/Key.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.common.termutils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Enum that represents some common keys
+ * @author Mattia Martelli
+ */
+public enum Key {
+    TAB,
+    RETURN,
+
+    // Both esc key and escape sequence
+    ESCAPE,
+
+    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW,
+
+    // Used as a generic key that does not need to be evaluated
+    GENERIC;
+
+    /**
+     * Parse the first key of the provided stream
+     * @param inputStream The stream to read from
+     * @return The first key read
+     * @throws IOException if an I/O error occurs
+     */
+    public static Key parseKey(InputStream inputStream) throws IOException {
+        return switch (inputStream.read()) {
+            case 9  -> TAB;
+            case 13 -> RETURN;
+
+            case 27 -> {
+                if (inputStream.available() == 0)
+                    yield  ESCAPE;
+
+                if (inputStream.read() != 79)
+                    yield GENERIC;
+
+                yield switch (inputStream.read()) {
+                    case 65 -> UP_ARROW;
+                    case 66 -> DOWN_ARROW;
+                    case 67 -> RIGHT_ARROW;
+                    case 68 -> LEFT_ARROW;
+
+                    default -> GENERIC;
+                };
+            }
+
+            default -> GENERIC;
+        };
+    }
+}
