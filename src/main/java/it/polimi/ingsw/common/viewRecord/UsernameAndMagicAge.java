95692b336d243f07263db29b90780635c8b3357f
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-29 22:03:01 +0200

    Added username sanitization and JavaDocs

--- src/main/java/it/polimi/ingsw/common/viewRecord/UsernameAndMagicAge.java
+++ src/main/java/it/polimi/ingsw/common/viewRecord/UsernameAndMagicAge.java
@@ -1,3 +1,18 @@
 package it.polimi.ingsw.common.viewRecord;
 
-public record UsernameAndMagicAge(String username, int magicAge) {}
+/**
+ * Record containing an username and magicAge of the player, along with a couple utility methods
+ * @param username A string containing the player's username
+ * @param magicAge An integer containing the player's magicAge
+ */
+public record UsernameAndMagicAge(String username, int magicAge) {
+
+    /**
+     * Return a trimmed, lower case of the passed string
+     * @param username The string to sanitize
+     * @return The sanitized string
+     */
+    public static String sanitizeUsername(String username){
+        return username.toLowerCase();
+    }
+}
