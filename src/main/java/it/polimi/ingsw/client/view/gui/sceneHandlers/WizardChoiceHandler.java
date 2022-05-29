b06b6103eb7e8b093cb6efb9caf08dbeba7ff4b6
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-05-29 08:57:07 +0200

    Added wizardChoice page and respective handler. Also added images

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/WizardChoiceHandler.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.client.view.gui.sceneHandlers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class WizardChoiceHandler {
+
+    @FXML
+    private AnchorPane wizardChoice_pane;
+
+    @FXML
+    private ImageView wizardChoice_imgView;
+
+    @FXML
+    private AnchorPane wizardChoice_subPane;
+
+    @FXML
+    private HBox wizardChoice_hbox;
+
+    @FXML
+    private AnchorPane wizard0_pane;
+
+    @FXML
+    private ImageView wizard0_img;
+
+    @FXML
+    private AnchorPane wizard1_pane;
+
+    @FXML
+    private ImageView wizard1_img;
+
+    @FXML
+    private AnchorPane wizard2_pane;
+
+    @FXML
+    private ImageView wizard2_img;
+
+    @FXML
+    private AnchorPane wizard3_pane;
+
+    @FXML
+    private ImageView wizard3_img;
+
+    @FXML
+    private Label wizardChoice_titleLabel;
+}
