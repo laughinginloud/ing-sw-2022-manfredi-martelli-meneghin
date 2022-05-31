82873b4489a56380b26c8896c509bc1848a108bf
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-06-01 00:19:33 +0200

    Added UnableToJoin Handler

--- src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/UnableToJoinHandler.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/UnableToJoinHandler.java
@@ -1,12 +1,13 @@
 package it.polimi.ingsw.client.view.gui.sceneHandlers;
 
+import it.polimi.ingsw.client.view.gui.ViewGUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
-public class UnableToJoinHandler {
+public class UnableToJoinHandler implements GUIHandler {
 
     @FXML
     private AnchorPane unableToJoin_pane;
@@ -19,4 +20,12 @@ public class UnableToJoinHandler {
 
     @FXML
     private Label unableToJoin_text;
+
+    /**
+     * @param gui
+     */
+    @Override
+    public void setGUI(ViewGUI gui) {
+        //TODO: [GUI implementation]
+    }
 }
