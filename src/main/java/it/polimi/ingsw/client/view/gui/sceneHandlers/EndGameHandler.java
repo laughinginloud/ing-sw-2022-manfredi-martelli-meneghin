8dc74fd1ed68e5aa306d6e6033e0a3f8831ce334
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-06-01 00:22:45 +0200

    Added EndGameHandler and PlayersSchoolBoardHandler

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/EndGameHandler.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.client.view.gui.sceneHandlers;
+
+import it.polimi.ingsw.client.view.gui.ViewGUI;
+
+public class EndGameHandler implements GUIHandler {
+    /**
+     * @param gui
+     */
+    @Override
+    public void setGUI(ViewGUI gui) {
+        //TODO: [GUI implementation]
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/PlayersSchoolBoardHandler.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.client.view.gui.sceneHandlers;
+
+import it.polimi.ingsw.client.view.gui.ViewGUI;
+
+public class PlayersSchoolBoardHandler implements GUIHandler {
+
+    /**
+     * @param gui
+     */
+    @Override
+    public void setGUI(ViewGUI gui) {
+        //TODO: [GUI implementation]
+    }
+}
