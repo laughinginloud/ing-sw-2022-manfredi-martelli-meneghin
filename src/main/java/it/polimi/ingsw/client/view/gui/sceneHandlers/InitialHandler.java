78080a2e4c9dec089fc9b2bba3999b6447d3c876
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-06-01 10:42:07 +0200

    Completed initialPage.fxml and added implementation in handler

--- src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/InitialHandler.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/InitialHandler.java
@@ -1,10 +1,13 @@
 package it.polimi.ingsw.client.view.gui.sceneHandlers;
 
+import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.view.gui.ViewGUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 
 public class InitialHandler implements GUIHandler{
     private ViewGUI gui;
@@ -20,13 +23,35 @@ public class InitialHandler implements GUIHandler{
     @FXML
     private Button play_button;
 
+    @FXML
+    private Label welcome_label;
+
+    @FXML
+    private ImageView cranio_img;
+
+    @FXML
+    private Rectangle polimi_background;
+
+    @FXML
+    private ImageView polimi_img;
+
+    @FXML
+    private Label love_label;
+
     // endregion FXML_Ids
 
-    // TODO [InitialScene] Implementation
+    /**
+     * Method called on the click of "play_button"
+     * Signals to the client that the player wants to play a new game
+     */
+    public void pressPlayButton() {
+        Client.signalPlayExit(true);
+    }
+
 
     /**
-     *
-     * @param gui
+     * Sets the ViewGUI at which the ServerInfoHandler is related
+     * @param gui the ViewGUI instance
      */
     @Override
     public void setGUI(ViewGUI gui) {
