a8b72d6f615e44879369781e8c6e8fea454f5711
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-06-01 11:10:44 +0200

    Added JavaDocs to GuiHandler

--- src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/GUIHandler.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/sceneHandlers/GUIHandler.java
@@ -2,9 +2,15 @@ package it.polimi.ingsw.client.view.gui.sceneHandlers;
 
 import it.polimi.ingsw.client.view.gui.ViewGUI;
 
-// TODO JavaDocs
-
+/**
+ * GUI Handler's (or controllers) interface
+ * Contains the method to set the ViewGUI they are linked with
+ */
 public interface GUIHandler {
 
+    /**
+     * Sets the ViewGUI at which the ServerInfoHandler is related
+     * @param gui the ViewGUI instance
+     */
     void setGUI(ViewGUI gui);
 }
