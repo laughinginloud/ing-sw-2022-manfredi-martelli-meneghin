7afceb985d18337d80c5de9fd8026fdb0814b70b
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-06-01 21:39:19 +0200

    Updated cranio directory logo

--- src/main/java/it/polimi/ingsw/client/view/gui/ViewGUI.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/ViewGUI.java
@@ -58,7 +58,7 @@ public final class ViewGUI extends Application implements View {
 
         // Sets the title, the scene, the icon
         stage.setTitle("Eriantys pre alpha 4.0");
-        stage.getIcons().add(new Image("cranio.png"));
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResource("it/polimi/ingsw/images/cranio.png").toString(), true));
 
         // Sets the trigger on close request. Event on close request -> exit
         stage.setOnCloseRequest(event -> {
