017e026971ead7bb7e6b5376ac0e8841ff5bae10
Mattia Martelli <mattia.martelli@live.it>
2022-06-25 12:31:24 +0200

    Methods refactor

--- src/main/java/it/polimi/ingsw/common/utils/Methods.java
+++ src/main/java/it/polimi/ingsw/common/utils/Methods.java
@@ -10,65 +10,10 @@ import static java.lang.Thread.currentThread;
  * @author Mattia Martelli
  */
 public /*static*/ final class Methods {
+
     private Methods() {}
 
-    /**
-     * Execute the action if the object is not null
-     * @param obj    The object to null-check
-     * @param action The action to perform
-     */
-    public static void ifNotNull(Object obj, Runnable action) {
-        if (obj != null)
-            action.run();
-    }
-
-    /**
-     * Consume the object if it is not null
-     * @param obj    The object to null-check
-     * @param action The consumer to activate
-     * @param <T>    The type of the object and the consumer
-     */
-    public static <T> void ifNotNull(T obj, Consumer<T> action) {
-        if (obj != null)
-            action.accept(obj);
-    }
-
-    /**
-     * Execute an action based on the state of the object
-     * @param obj       The object to null-check
-     * @param ifNotNull The action to perform if the object is not null
-     * @param ifNull    The action to perform it the object is null
-     */
-    public static void ifNotNullOrElse(Object obj, Runnable ifNotNull, Runnable ifNull) {
-        if (obj != null)
-            ifNotNull.run();
-
-        else
-            ifNull.run();
-    }
-
-    /**
-     * Execute an action based on the state of the object
-     * @param obj       The object to null-check
-     * @param ifNotNull The action to perform if the object is not null
-     * @param ifNull    The action to perform it the object is null
-     */
-    public static <T> void ifNotNullOrElse(T obj, Consumer<T> ifNotNull, Runnable ifNull) {
-        if (obj != null)
-            ifNotNull.accept(obj);
-
-        else
-            ifNull.run();
-    }
-
-    /**
-     * Capitalize the first letter of the string, whilst putting in lower case the rest
-     * @param str The string to capitalize
-     * @return    The capitalized string
-     */
-    public static String capitalize(String str) {
-        return Character.toUpperCase(str.charAt(0)) + str.substring(1).toLowerCase();
-    }
+    // region Array copy
 
     /**
      * Deep copy an array
@@ -107,6 +52,71 @@ public /*static*/ final class Methods {
         return Arrays.copyOf(array, array.length);
     }
 
+    // endregion
+
+    // region Null check
+
+    /**
+     * Execute the action if the object is not null
+     * @param obj    The object to null-check
+     * @param action The action to perform
+     */
+    public static void ifNotNull(Object obj, Runnable action) {
+        if (obj != null)
+            action.run();
+    }
+
+    /**
+     * Consume the object if it is not null
+     * @param obj    The object to null-check
+     * @param action The consumer to activate
+     * @param <T>    The type of the object and the consumer
+     */
+    public static <T> void ifNotNull(T obj, Consumer<T> action) {
+        ifNotNull(obj, () -> action.accept(obj));
+    }
+
+    /**
+     * Execute an action based on the state of the object
+     * @param obj       The object to null-check
+     * @param ifNotNull The action to perform if the object is not null
+     * @param ifNull    The action to perform it the object is null
+     */
+    public static void ifNotNullOrElse(Object obj, Runnable ifNotNull, Runnable ifNull) {
+        if (obj != null)
+            ifNotNull.run();
+
+        else
+            ifNull.run();
+    }
+
+    /**
+     * Execute an action based on the state of the object
+     * @param obj       The object to null-check
+     * @param ifNotNull The action to perform if the object is not null
+     * @param ifNull    The action to perform it the object is null
+     */
+    public static <T> void ifNotNullOrElse(T obj, Consumer<T> ifNotNull, Runnable ifNull) {
+        ifNotNullOrElse(obj, () -> ifNotNull.accept(obj), ifNull);
+    }
+
+    // endregion
+
+    // region String
+
+    /**
+     * Capitalize the first letter of the string, whilst putting in lower case the rest
+     * @param str The string to capitalize
+     * @return    The capitalized string
+     */
+    public static String capitalize(String str) {
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1).toLowerCase();
+    }
+
+    // endregion
+
+    // region Thread
+
     /**
      * Check whether the current thread has been interrupted
      * @return <code>true</code> if the thread has been interrupted, <code>false</code> otherwise
@@ -132,4 +142,7 @@ public /*static*/ final class Methods {
     public static <T> void async(Consumer<T> action, T param) {
         async(() -> action.accept(param));
     }
+
+    // endregion
+
 }
