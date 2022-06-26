a0b35c3ade954334b24c1ebb0d3ddbe9f46ba3a9
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2022-06-26 23:13:47 +0200

    Fixed AssistantCard research method in GameStatePlayCard

--- src/main/java/it/polimi/ingsw/server/controller/state/GameStateThread.java
+++ src/main/java/it/polimi/ingsw/server/controller/state/GameStateThread.java
@@ -75,23 +75,6 @@ public final class GameStateThread extends Thread {
             state instanceof GameStateEndGame)
             return;
 
-        // If the game has been terminated on a completely automatic state,
-        // simply execute it, go to the next and try to save that
-        if (state instanceof GameStateComputeIsland ||
-            state instanceof GameStateEndCheckPhase ||
-            state instanceof GameStateFillClouds    ||
-            state instanceof GameStateSelectTurnOrder) {
-            try {
-                state.executeState();
-                save(state.nextState());
-            }
-
-            // NB: this catch is here to please the type system, since the possible states
-            // do not throw it, but GameState.executeState()'s signature has it because
-            // most states do, since they send and receive requests
-            catch (SocketException ignored) {}
-        }
-
         try {
             GameSave.saveGame(fileName, state);
         }
