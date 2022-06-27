788bb7c7cbb9f1419256c60e780b58613e9f66a5
Mattia Martelli <mattia.martelli@live.it>
2022-06-27 23:25:38 +0200

    GameActions and GameValues refactor

--- src/main/java/it/polimi/ingsw/common/GameActions.java
+++ src/main/java/it/polimi/ingsw/common/GameActions.java
@@ -1,74 +1,124 @@
 package it.polimi.ingsw.common;
 
+/**
+ * Actions that can be performed by the player
+ * @author Sebastiano Meneghin
+ */
 public enum GameActions {
-    // Contains an array of characterCard playable by the current player
+    /**
+     * Contains an array of characterCard playable by the current player
+     */
     ENDTURN,
 
-    // A signal sent by the client if the player decide to end his turn instead of play a characterCard
+    /**
+    * A signal sent by the client if the player decide to end his turn instead of play a characterCard
+    */
     ENDTHISTURN,
 
-    // Contains the cloudTiles that can be chosen by the player, at the end of his ActonPhase
+    /**
+    * Contains the cloudTiles that can be chosen by the player, at the end of his ActonPhase
+    */
     CHOOSECLOUD,
 
-    // An int representing the cloudTile chosen by the player during GameStateChooseCloud
+    /**
+    * An int representing the cloudTile chosen by the player during GameStateChooseCloud
+    */
     SELECTEDCLOUD,
 
-    // CHOOSECLOUD + Array of characterCard playable by the current player
+    /**
+    * CHOOSECLOUD + Array of characterCard playable by the current player
+    */
     CHOOSECLOUDORPLAYCARD,
 
-    // Contains the students that can be moved and the DiningRoom where they can be moved
+    /**
+    * Contains the students that can be moved and the DiningRoom where they can be moved
+    */
     MOVESTUDENT,
 
-    // MOVESTUDENT + Array of characterCard playable by the current player
+    /**
+    * MOVESTUDENT + Array of characterCard playable by the current player
+    */
     MOVESTUDENTORPLAYCARD,
 
-    // Information about the movement of the student
+    /**
+    * Information about the movement of the student
+    */
     MOVESTUDENTINFO,
 
-    // Contains the maximum range that motherNature can be moved in
+    /**
+    * Contains the maximum range that motherNature can be moved in
+    */
     MOVEMOTHERNATURE,
 
-    // MOVEMOTHERNATURE + Array of characterCard playable by the current player
+    /**
+    * MOVEMOTHERNATURE + Array of characterCard playable by the current player
+    */
     MOVEMOTHERNATUREORPLAYCARD,
 
-    // The mother nature movement selected by the player
+    /**
+    * The mother nature movement selected by the player
+    */
     CHOSENMOTHERNATUREMOVEMENT,
 
-    // Array of assistantCard playable by the current player
+    /**
+    * Array of assistantCard playable by the current player
+    */
     PLAYASSISTANTCARD,
 
-    // The AssistantCard chosen by the player during his PlanPhase turn
+    /**
+    * The AssistantCard chosen by the player during his PlanPhase turn
+    */
     CHOSENASSISTANTCARD,
 
-    // Character of the characterCard that has been played by the player
+    /**
+    * Character of the characterCard that has been played by the player
+    */
     CHOSENCHARACTER,
 
-    // Fields related to the characterCard utilization
+    /**
+    * Fields related to the characterCard utilization
+    */
     CHARACTERCARDEFFECT,
 
-    // Request whether a game should be loaded
+    /**
+    * Request whether a game should be loaded
+    */
     LOADGAME,
 
-    // A boolean representing the player's choice to resume or not resume an old game
+    /**
+    * A boolean representing the player's choice to resume or not resume an old game
+    */
     LOADGAMECHOICE,
 
-    // Used to ask the player his username
+    /**
+    * Used to ask the player his username
+    */
     USERNAMEANDMAGICAGE,
 
-    // The username inserted by the player
+    /**
+    * The username inserted by the player
+    */
     INSERTEDUSERNAMEANDAGE,
 
-    // Setup phase requests
+    /**
+    * Setup phase requests
+    */
     RULES,
 
     WIZARD,
 
-    // A map(GameValues, Object) containing all the fields' values chosen by the player
+    /**
+    * A map(GameValues, Object) containing all the fields' values chosen by the player
+    */
     CHOSENFIELDSMAP,
 
-    // A record "GameRules" containing the rules chosen by the player (numOfPlayer and expertMode)
+    /**
+    * A record "GameRules" containing the rules chosen by the player (numOfPlayer and expertMode)
+    */
     CHOSENRULES,
 
-    // The wizard chosen by the player from the availableWizard
+    /**
+    * The wizard chosen by the player from the availableWizard
+    */
     CHOSENWIZARD,
 }
--- src/main/java/it/polimi/ingsw/common/GameValues.java
+++ src/main/java/it/polimi/ingsw/common/GameValues.java
@@ -1,151 +1,187 @@
 package it.polimi.ingsw.common;
 
 public enum GameValues {
-    // An array of Islands
-    // USED IN MAP
+    /**
+    * An array of Islands
+    */
     ISLANDARRAY,
 
-    // An array of Players
-    // USED IN MAP
+    /**
+    * An array of Players
+    */
     PLAYERARRAY,
 
-    // An array of CloudTiles
-    // USED IN MAP
+    /**
+    * An array of CloudTiles
+    */
     CLOUDARRAY,
 
-    // An array of CharacterCards
-    // USED IN MAP
+    /**
+    * An array of CharacterCards
+    */
     CHARACTERCARDARRAY,
 
-    // A GameModel representing the GameModel present on the Server's Controller
-    // USED IN MAP
+    /**
+    * A GameModel representing the GameModel present on the Server's Controller
+    */
     MODEL,
 
-    // An int representing the position of the characterCard into the model's characterCardArray
-    // USED IN MAP
+    /**
+    * An int representing the position of the characterCard into the model's characterCardArray
+    */
     CHARACTERCARDPOSITION,
 
-    // An enumeration constant of PlayCharacterAction correspondent to the character on a CharacterCard
-    // USED IN MAP
+    /**
+    * An enumeration constant of PlayCharacterAction correspondent to the character on a CharacterCard
+    */
     CHARACTERVALUE,
 
-    // An array of color (students or professor)
-    // USED IN MAP
+    /**
+    * An array of color (students or professor)
+    */
     COLORARRAY,
 
-    // An array of boolean of various lengths
-    // USED IN MAP
+    /**
+    * An array of boolean of various lengths
+    */
     BOOLARRAY,
 
-    // An array of Islands, where MotherNature can be moved on
-    // USED IN MAP
+    /**
+    * An array of Islands, where MotherNature can be moved on
+    */
     MNPOSSIBLEMOVEMENTS,
 
-    // An int representing the position of MotherNature
-    // USED IN MAP
+    /**
+    * An int representing the position of MotherNature
+    */
     MOTHERNATURE,
 
-    // A tuple containing:
-    //  - left: a player's ID
-    //  - right: the entrance of the player
-    // USED IN MAP
+    /**
+    * A tuple containing:
+    *  - left: a player's ID
+    *  - right: the entrance of the player
+    */
     ENTRANCE,
 
-    // An array of Entrances, containing all the players' entrances
-    // USED IN MAP
+    /**
+    * An array of Entrances, containing all the players' entrances
+    */
     ENTRANCEARRAY,
 
-    // A tuple containing:
-    //  - left: a player's ID
-    //  - right: the dining room of the player
-    // USED IN MAP
+    /**
+    * A tuple containing:
+    *  - left: a player's ID
+    *  - right: the dining room of the player
+    */
     DININGROOM,
 
-    // An array of DiningRooms, containing all the players' diningRooms
-    // USED IN MAP
+    /**
+    * An array of DiningRooms, containing all the players' diningRooms
+    */
     DININGROOMARRAY,
 
-    // An array of SchoolBoard, containing all the players' schoolBoards
-    // USED IN MAP
+    /**
+    * An array of SchoolBoard, containing all the players' schoolBoards
+    */
     SCHOOLBOARDARRAY,
 
-    // The GlobalProfessorTable, linked to the class GameModel
-    // USED IN MAP
+    /**
+    * The GlobalProfessorTable, linked to the class GameModel
+    */
     GLOBALPROFESSORTABLE,
 
-    // The value of the global CoinPool, present only in ExpertMode
-    // USED IN MAP
+    /**
+    * The value of the global CoinPool, present only in ExpertMode
+    */
     COINPOOL,
 
-    // An int indicating the index of the student the client selected - CharacterCard 'MONK', 'PRINCESS',
-    // USED IN MAP
+    /**
+    * An int indicating the index of the student the client selected - CharacterCard 'MONK', 'PRINCESS',
+    */
     STUDENTINDEX,
 
-    // An int that indicates the index of the island that client selected - CharacterCard 'MONK', 'HERBALIST',
-    // 'STANDARD_BEARER'
-    // USED IN MAP
+    /**
+    * An int that indicates the index of the island that client selected - CharacterCard 'MONK', 'HERBALIST',
+    * 'STANDARD_BEARER'
+    */
     ISLANDINDEX,
 
-    // An int representing the max number of students that can be moved using the characterCard Jester
-    // USED IN MAP
+    /**
+    * An int representing the max number of students that can be moved using the characterCard Jester
+    */
     MAXMOVEMENTJESTER,
 
-    // An int representing the number of students that will be moved by the player using the characterCard Jester
-    // USED IN MAP
+    /**
+    * An int representing the number of students that will be moved by the player using the characterCard Jester
+    */
     MOVEMENTJESTER,
 
-    // An Array of Color representing the students present on the Entrance of a player
-    // USED IN MAP
+    /**
+    * An Array of Color representing the students present on the Entrance of a player
+    */
     ENTRANCESTUDENTS,
 
-    // An Array of Color representing the students present on a characterCard - CharacterCard 'PRINCESS'
-    // USED IN MAP
+    /**
+    * An Array of Color representing the students present on a characterCard - CharacterCard 'PRINCESS'
+    */
     CARDSTUDENTS,
 
-    // An int that indicates the index of the student the player selected from the entrance - CharacterCard 'JESTER'
-    // USED IN MAP
+    /**
+    * An int that indicates the index of the student the player selected from the entrance - CharacterCard 'JESTER'
+    */
     ENTRANCESTUDENTINDEX,
 
-    // An int representing the index of the student the player selected from the card's students - CharacterCard 'JESTER'
-    // USED IN MAP
+    /**
+    * An int representing the index of the student the player selected from the card's students - CharacterCard 'JESTER'
+    */
     CARDSTUDENTINDEX,
 
-    // An int representing the max number of students that can be moved using the characterCard Bard
-    // USED IN MAP
+    /**
+    * An int representing the max number of students that can be moved using the characterCard Bard
+    */
     MAXMOVEMENTBARD,
 
-    // An int representing the number of students that will be moved by the player using the characterCard Bard
-    // USED IN MAP
+    /**
+    * An int representing the number of students that will be moved by the player using the characterCard Bard
+    */
     MOVEMENTBARD,
 
-    // An array of Color representing the entrances' students that can be swapped with diningRoom's students
-    // by the player currently playing the characterCard 'BARD'
-    // USED IN MAP
+    /**
+    * An array of Color representing the entrances' students that can be swapped with diningRoom's students
+    * by the player currently playing the characterCard 'BARD'
+    */
     BARDSWAPPABLESTUDENTS,
 
-    // A Map(Color, Boolean[]) that representing which diningRoom's student can be swapped with a selected entrance's student (which is the key)
-    // USED IN MAP
+    /**
+    * A Map(Color, Boolean[]) that representing which diningRoom's student can be swapped with a selected entrance's student (which is the key)
+    */
     BARDSWAPMAP,
 
-    // A Color representing the Color of the diningRoomTable containing the student that the player
-    // want to swap the swappableStudent with - CharacterCard 'BARD'
-    // USED IN MAP
+    /**
+    * A Color representing the Color of the diningRoomTable containing the student that the player
+    * want to swap the swappableStudent with - CharacterCard 'BARD'
+    */
     DININGROOMTABLECOLOR,
 
-    // A Color representing the color the player wants to inhibit - CharacterCard 'MERCHANT'
-    // USED IN MAP
+    /**
+    * A Color representing the color the player wants to inhibit - CharacterCard 'MERCHANT'
+    */
     MERCHANTCOLOR,
 
-    // An array of color that represent the color of the diningRoomTable from where students can be reduced - CharacterCard 'THIEF'
+    /**
+    * An array of color that represent the color of the diningRoomTable from where students can be reduced - CharacterCard 'THIEF'
+    */
     REDUCIBLECOLOR,
 
-    // A color, chosen by the player, representing the student of the students that will be
-    // reduced in the player's diningRooms - CharacterCard 'THIEF'
-    // USED IN MAP
+    /**
+    * A color, chosen by the player, representing the student of the students that will be
+    * reduced in the player's diningRooms - CharacterCard 'THIEF'
+    */
     REDUCECOLOR,
 
-    // A String representing the string that the server will send to the client as a confirmation that
-    // the CharacterCard effect has been correctly applied - CharacterCard 'CAVALIER', 'CENTAUR', 'MAGICIAN', 'MERCHANT'
-    // USED IN MAP
-    CONFIRMATIONSTRING,
+    /**
+    * A String representing the string that the server will send to the client as a confirmation that
+    * the CharacterCard effect has been correctly applied - CharacterCard 'CAVALIER', 'CENTAUR', 'MAGICIAN', 'MERCHANT'
+    */
+    CONFIRMATIONSTRING
 }
