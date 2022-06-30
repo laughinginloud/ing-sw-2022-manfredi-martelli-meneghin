18aade61e45d44915bcb275a3570de8236157ce8
Mattia Martelli <mattia.martelli@live.it>
2022-06-30 16:59:09 +0200

    Save refactor

--- src/main/java/it/polimi/ingsw/server/controller/save/SaveData.java
+++ src/main/java/it/polimi/ingsw/server/controller/save/SaveData.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.server.controller.state.GameState;
 
 /**
  * Record that represents the serialized saved data
+ * @param controllerData The ControllerData instance
+ * @param gameState      The next state of the DFA, to be executed at the start of the loaded game
  * @author Mattia Martelli
  */
 public record SaveData (ControllerData controllerData, GameState gameState) {}
